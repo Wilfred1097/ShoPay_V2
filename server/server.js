211f6601247883c514a0f337d07e69d88f313289
Wilfredo D. Catalan <99998916+Wilfred1097@users.noreply.github.com>
2023-12-21 02:07:29 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ["POST", "GET"],
     credentials: true
 }));
